889d1782c6b43b387af0dd5e01a61e74e5c67746
“byronrmrz” <byronrmrz@gmail.com>
2025-03-08 13:52:03 -0600

    [ADD]Nextjs project, frontend base, call fetch to backend from frontend

--- habits-tracker-backend/config/database.js
+++ habits-tracker-backend/config/database.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 require('dotenv').config ();
 const express = require('express');
 
-// // mongoose.connect(process.env.MONGO_URI,{
-// //     useNewUrlParser: true,
-// //     useUnifiedTopology: true
-// // }).then(()=> {
-// //     console.log('Connected to MongoDB');
-// // }).catch((err)=>{
-// //     console.log('Error connecting to MongoDB', err);
-// // });
+// mongoose.connect(process.env.MONGO_URI,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(()=> {
+//     console.log('Connected to MongoDB');
+// }).catch((err)=>{
+//     console.log('Error connecting to MongoDB', err);
+// });
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Conectado a MongoDB"))
